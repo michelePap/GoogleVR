5b53f0c5e3ea176fec1217cbd276b590d1a326c3
Mic <mikele91.xeno@gmail.com>
2017-12-10 17:27:03 +0100

    add customviewer

---
+++ scripts/vrview/build/custom_viewer.js
@@ -0,0 +1,14 @@
+Viewers = {
+  CardboardV1: new CardboardViewer({
+    id: 'CardboardV3',
+    label: 'Cardboard Test',
+    fov: 40,
+    interLensDistance: 0.060,
+    baselineLensDistance: 0.035,
+    screenLensDistance: 0.042,
+    distortionCoefficients: [0.441, 0.156],
+    inverseCoefficients: [-0.4410035, 0.42756155, -0.4804439, 0.5460139,
+      -0.58821183, 0.5733938, -0.48303202, 0.33299083, -0.17573841,
+      0.0651772, -0.01488963, 0.001559834]
+  })
+};
